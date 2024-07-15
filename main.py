9a70c564efc331302d8926c567bcc8a2c5b8442d
WalterMoran24 <168034559+WalterMoran24@users.noreply.github.com>
2024-07-15 17:44:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+'''Inicio del programa'''
+
 from codebreaker import Codebreaker
 
-intentos_totales = 10
+INTENTOS_TOTALES = 10
 codebreaker = Codebreaker()
 
-intento = 0
+INTENTO = 0
 
 print('Jugar Codebreaker!')
 
-while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
-
-
+while INTENTO != INTENTOS_TOTALES:
+    number = input('Numero:')
+    resolve = codebreaker.adivinar(number)
+    print(resolve)
+    if resolve is True:
+        print('You win!!')
+        break
